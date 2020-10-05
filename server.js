359d779eeb1f2faae53f9573ff18401aaa586739
Archana Shokeen <archanashokeen@Archanas-MacBook-Air.local>
2020-10-05 15:14:18 -0700

    html views and styling

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
-app.get("/", function (req, res) {
-  res.send("server is up");
-});
+
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.get("/", function (req, res) {
+  res.render("main");
+});
+app.get("/createblog", function (req, res) {
+  res.render("createBlog");
+});
+
 app.listen(3000);
